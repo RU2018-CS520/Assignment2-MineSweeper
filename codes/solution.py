340d34f984bac4c56867cc0deba2adca0060a422
yhz012 <603907639@qq.com>
2018-10-15 12:34:38 +0800

    2 bugs: m.left over-decrease, chain unexpected stoped

---
+++ codes/solution.py
@@ -0,0 +1,191 @@
+import frame
+
+class player(object):
+	def __init__(self, m, chain = False):
+		self.m = m
+		self.chain = chain
+		self.alive = True
+		self.safeList = []
+		self.startList = [(0, 0), (self.m.rows-1, 0), (0, self.m.cols-1), (self.m.rows-1, self.m.cols-1), (0, self.m.cols//2), (self.m.rows//2, 0), (self.m.rows//2, self.m.cols-1), (self.m.rows-1, self.m.cols//2)]
+		return
+
+
+	#if (row, col) no warn or no left, all neighbor are conclusive
+	def updateChain(self, row, col, iNebr = None):
+		if self.alive:
+			chainFlag = False
+			if self.m.warn[row, col] == 0: #no warn, all unexplored neighbor safe
+				chainFlag = self.setNeighborSafe(row, col, iNebr)
+				chainFlag = True
+			elif self.m.left[row, col] == self.m.warn[row, col]: #no inconclusive, all unexplored neighbor flag
+				chainFlag = self.setNeighborFlag(row, col, iNebr)
+				chainFlag = True
+			return chainFlag
+
+		else: #dead
+			return False
+
+	#empty function, chain to check new flag block's  neighbor's warns
+	def updateFlagBlock(self, row, col, iNebr = None):
+		if self.alive:
+			chainFlag = False
+			if self.m.left[row, col] == 0: #all neighbor explored or flaged, done
+				self.m.done[row, col] = True
+
+			else: #still neighbor
+				if self.chain:
+					neighbor = self.checkInNeighbor(row, col, iNebr)
+					for pos in neighbor:
+						chainFlag = chainFlag or self.updateChain(row, col, iNebr = neighbor)
+			return chainFlag
+
+		else: #dead
+			return False
+
+	#update new hint block's warn
+	def updateHintBlock(self, row, col, iNebr = None):
+		if self.alive:
+			chainFlag = False
+			if self.m.left[row, col] == 0: #all neighbor explored or flaged, done
+				self.m.done[row, col] = True
+
+			else: #still neighbor, update this block's warn
+				flagCount, neighbor = self.m.count(row, col, 'flag', iNebr = iNebr, oNebr = True)
+				self.m.warn[row, col] = self.m.hint[row, col] - flagCount
+
+				if self.chain:
+					chainFlag = self.updateChain(row, col, iNebr = neighbor)
+			return chainFlag
+
+		else: #dead
+			return False
+
+	#set (row, col) flag
+	def setBlockFlag(self, row, col, iNebr = None):
+		if self.alive:
+			self.m.flag[row, col] = True
+			chainFlag = False
+			neighbor = self.checkInNeighbor(row, col, iNebr)
+			for pos in neighbor:
+				if not self.m.covered[pos]: #decreas explored neighbor's warn
+					self.m.warn[pos] = self.m.warn[pos] - 1
+
+					if self.chain:
+						neighbor = self.updateFlagBlock(row, col)
+			return chainFlag
+
+		else: #dead
+			return False
+
+
+	#set (row, col) safe
+	def setBlockSafe(self, row, col, iNebr = None):
+		if self.alive:
+			self.m.safe[row, col] = True
+
+			chainFlag = False
+			if self.chain: #explore this block and update this block
+				hint, chainFlag, neighbor = self.exploreBlock(row, col, iNebr = iNebr, oNebr = True)
+				if hint is False: #dead
+					return False
+				else:
+					chainFlag = chainFlag or self.updateHintBlock(row, col, iNebr = neighbor)
+			else: #add to safeList waiting to be explored
+				self.safeList.append((row, col))
+			return chainFlag
+
+		else: #dead
+			return False
+
+	def setNeighborFlag(self, row, col, iNebr = None):
+		if self.alive:
+			neighbor = self.checkInNeighbor(row, col, iNebr)
+			chainFlag = False
+			for pos in neighbor:
+				if self.m.covered[pos] and not self.m.flag[pos]: #set unexplored block flag
+					chainFlag = chainFlag or self.setBlockFlag(pos[0], pos[1])
+			return chainFlag
+		else: #dead
+			return False
+
+	#set all unexplored neighbor of (row, col) safe
+	def setNeighborSafe(self, row, col, iNebr = None):
+		if self.alive:
+			neighbor = self.checkInNeighbor(row, col, iNebr)
+			chainFlag = False
+			for pos in neighbor:
+				if self.m.covered[pos] and not self.m.flag[pos]: #set unexplored block safe
+					chainFlag = chainFlag or self.setBlockSafe(pos[0], pos[1])
+			return chainFlag
+		else: #dead
+			return False
+
+	#explore (row, col) and update neighbor's left
+	def exploreBlock(self, row, col, iNebr = None, oNebr = False):
+		print((row, col))
+		if self.alive:
+			tempHint = self.m.explore(row, col, blind = self.m.blind, optimistic = self.m.optimistic, cautious = self.m.cautious)
+			chainFlag = False
+			if tempHint is False:
+				print('E: solution.player.exploreBlock. What a Terrible Failure!')
+				self.alive = False
+			#	self.m.visualize(cheat = False)
+			#	self.m.visualize(cheat = True)
+				neighbor = iNebr
+
+			else: #get a new hint, update neighbor left #TODO: blind return process
+				self.m.hint[row, col] = tempHint
+				neighbor = self.checkInNeighbor(row, col, iNebr)
+				for pos in neighbor:
+					self.m.left[pos] = self.m.left[pos] - 1
+
+					if self.chain:
+						self.updateChain(pos[0], pos[1])
+
+		else: #dead
+			tempHint = False
+			chainFlag = False
+			neighbor = iNebr
+		
+		#return
+		if oNebr:
+			return (tempHint, chainFlag, neighbor)
+		else:
+			return (tempHint, chainFlag)
+
+
+	#tool function
+	def checkInNeighbor(self, row, col, iNebr):
+		if iNebr is None:
+			neighbor = self.m.getNeighbor(row, col)
+		else:
+			neighbor = iNebr
+		return neighbor
+
+def makeAMove(p):
+	startPos = p.startList.pop()
+	tempHint = p.m.start(*startPos)
+	
+	chainFlag = False
+	tempPos = startPos
+	while not chainFlag:
+		print(tempPos)
+		print(chainFlag)
+		chainFlag = p.updateChain(tempPos[0], tempPos[1])
+		tempPos = p.startList.pop()
+		tempHint, tempChainFlag, neighbor = p.exploreBlock(*tempPos, oNebr = True)
+		if tempHint is False:
+			return False
+		else:
+			chainFlag = chainFlag or tempChainFlag
+			chainFlag = chainFlag or p.updateHintBlock(*tempPos, iNebr = neighbor)
+
+
+if __name__ == '__main__':
+	m = frame.board(10, 10, 10)
+	p = player(m, chain = True)
+	res = makeAMove(p)
+	print(res)
+	m.visualize()
+	m.visualize(cheat = True)
+	print(m.left)
