d4bb2b6c99121269bfd4a22c2706d1574f7eab5c
yhz012 <603907639@qq.com>
2018-10-17 13:48:47 +0800

    bug fix prob

--- codes/solution.py
+++ codes/solution.py
@@ -9,13 +9,16 @@ class player(object):
 		self.chain = chain
 		self.alive = True
 		self.startList = [(0, 0), (self.m.rows-1, 0), (0, self.m.cols-1), (self.m.rows-1, self.m.cols-1), (0, self.m.cols//2), (self.m.rows//2, 0), (self.m.rows//2, self.m.cols-1), (self.m.rows-1, self.m.cols//2)]
+		self.subStartList = [(x, 0) for x in range(2, self.m.rows-2)] + [(x, self.m.cols-1) for x in range(2, self.m.rows-2)] + [(0, y) for y in range(2, self.m.rows-2)] + [(self.m.rows-1, y) for y in range(2, self.m.rows-2)]
 		self.prob = np.full((self.m.rows, self.m.cols, 9), np.nan, dtype = np.float16)
 
 		self.safeWaiting = set()
 		self.flagWaiting = set()
+		self.override = []
 
 		return
 
+	#basic action
 	#update warn
 	def updateHintBlock(self, row, col, iNebr = None):
 		flagCount, neighbor = self.m.count(row, col, 'flag', iNebr = iNebr, oNebr = True)
@@ -34,7 +37,8 @@ class player(object):
 				self.updateHintBlock(row, col, iNebr = neighbor)
 				self.updateNeighborP(row, col, iNebr = neighbor)
 				for pos, index in neighbor:
-					self.updateNeighborP(pos[0], pos[1])
+					if not self.m.covered[pos]:
+						self.updateNeighborP(pos[0], pos[1])
 			return True
 		else:
 			return False
@@ -52,7 +56,10 @@ class player(object):
 			self.prob[row, col] = 1 #TODO: should it be set 1? if optimistic/cautious?
 			neighbor = self.checkInNeighbor(row, col, iNebr)
 			for pos, index in neighbor:
-				self.m.left[pos[0], pos[1]] = self.m.left[pos[0], pos[1]] - 1
+				self.m.nebr[pos] = self.m.nebr[pos] // self.m.hashCore[row%5, col%5]
+				self.m.left[pos] = self.m.left[pos] - 1
+				if self.m.left[pos] == 0:
+					self.m.done[pos] = True
 				if not self.m.covered[pos]:
 					self.updateFlagBlock(pos[0], pos[1])
 					self.updateNeighborP(pos[0], pos[1])
@@ -60,7 +67,7 @@ class player(object):
 		else:
 			return False
 
-
+	#tool function
 	#return neighbor
 	def checkInNeighbor(self, row, col, iNebr):
 		if iNebr is None:
@@ -86,7 +93,10 @@ class player(object):
 				self.prob[row, col] = 0
 				neighbor = self.checkInNeighbor(row, col, iNebr)
 				for pos, index in neighbor:
+					self.m.nebr[pos[0], pos[1]] = self.m.nebr[pos[0], pos[1]] // self.m.hashCore[row%5, col%5]
 					self.m.left[pos] = self.m.left[pos] - 1
+					if self.m.left[pos] == 0:
+						self.m.done[pos] = True
 
 		else: #dead
 			tempHint = False
@@ -101,9 +111,6 @@ class player(object):
 	#calculate neighbor's prob
 	def updateNeighborP(self, row, col, iNebr = None):
 		if self.m.done[row, col]:
-			return False
-		if self.m.left[row, col] == 0:
-			self.m.done[row, col] = True
 			return False
 		
 		neighbor = self.checkInNeighbor(row, col, iNebr = iNebr)
@@ -120,39 +127,107 @@ class player(object):
 					chainFlag = True
 		return chainFlag
 
-#stat the game
-def firstStep(p):
-	startPos = p.startList.pop()
-	tempHint = p.m.start(*startPos)
-	p.updateNeighborP(*startPos)
+	#a block's effective neighbors are all his parent's effective neighbors
+	def checkOverride(self):
+		self.override = []
+		for row in self.m.rows:
+			for col in self.m.cols:
+				if self.m.done[row, col]:
+					continue
+				childList = []
+				for childRow in range(row-2, row+2):
+					if childRow > self.m.rows-1 or childRow < 0:
+						continue
+					for childCol in range(col-2, col+2):
+						if childCol > self.m.cols-1 or childCol < 0:
+							continue
+						if self.m.done[childRow, childCol]:
+							continue
+						if self.m.nebr[row, col] % self.m.nebr[childRow, childCol] == 0:
+							childList.append((childRow, childCol))
+				if childList:
+					self.override.append((len(childList), (row, col), childList))
+		return self.override
 
-	while p.startList and not (p.safeWaiting or p.flagWaiting):
-		tempPos = p.startList.pop()
-		p.hintSafeBlock(*tempPos)
-	return
+	def getNext(self):
+		completeRate = (self.m.blockCount + self.m.flagCount) / (self.m.rows * self.m.cols)
+		defaultProb = (self.m.mines - self.m.flagCount) / (self.m.rows * self.m.cols - self.m.blockCount)
+		tempProb = np.copy(self.prob)
+		tempProb[np.isnan(tempProb)] = defaultProb
+		meanProb = tempProb.mean(axis = 2)
+		#best start block, not bad block before endgame
+		if completeRate < 0.33:
+			while self.startList:
+				pos = self.startList.pop()
+				if self.m.covered[pos] and not self.m.flag[pos]:
+					return pos, meanProb[pos]
+		#better start block, bad block in middle stage
+		if completeRate < 0.1:
+			while self.subStartList:
+				pos = self.subStartList.pop()
+				if self.m.covered[pos] and not self.m.flag[pos]:
+					return pos, meanProb[pos]
+		#best block in middle stage and endgame
+		meanProb[(~self.m.covered) | self.m.flag] = 1
+		pos = np.unravel_index(np.argmin(meanProb), meanProb.shape)
+		return pos, meanProb[pos]
 
-#keep looping with bookkeeping
-def stepByStep(p):
-	while p.alive and (p.safeWaiting or p.flagWaiting):
-		while p.alive and p.safeWaiting:
-			tempPos = p.safeWaiting.pop()
-			p.hintSafeBlock(*tempPos)
-		while p.alive and p.flagWaiting:
-			tempPos = p.flagWaiting.pop()
-			p.flagMineBlock(*tempPos)
-	return p.alive
-		
+
+	#hi-level action
+	#stat the game
+	def firstStep(self):
+		startPos = self.startList.pop()
+		tempHint = self.m.start(*startPos)
+		self.updateNeighborP(*startPos)
+	
+		while self.alive and not (self.safeWaiting or self.flagWaiting):
+			self.stepOut()
+		return self.alive
+	
+	#1st arm, keep looping with bookkeeping
+	def stepByStep(self):
+		while self.alive and (self.safeWaiting or self.flagWaiting):
+			while self.alive and self.safeWaiting:
+				tempPos = self.safeWaiting.pop()
+				self.hintSafeBlock(*tempPos)
+			while self.alive and self.flagWaiting:
+				tempPos = self.flagWaiting.pop()
+				self.flagMineBlock(*tempPos)
+		return self.alive
+
+	#2nd arm, solve A+B=1 A+B+C=2
+	def solveOverride(self):
+		pass
+
+	#final arm, ready to die
+	def stepOut(self):
+		step, prob = self.getNext()
+		print(step)
+		print('W: solution.player.stepOut. risk (%d, %d)' %(step[0], step[1]))
+		if prob * 2 < 1:
+			self.hintSafeBlock(*step)
+		else:
+			self.flagMineBlock(*step)
+		return self.alive
+
+
+
 
 
 if __name__ == '__main__':
 	m = frame.board(64, 64, 512)
 	p = player(m, chain = True)
-	res = firstStep(p)
-	res = stepByStep(p)
-	print(res)
+	res = p.firstStep()
+	while p.alive and ((p.m.blockCount + p.m.flagCount) < (p.m.rows * p.m.cols)) and (p.m.flagCount < p.m.mines):
+		res = p.stepByStep()
+		while p.alive and ((p.m.blockCount + p.m.flagCount) < (p.m.rows * p.m.cols)) and (p.m.flagCount < p.m.mines) and not (p.safeWaiting or p.flagWaiting):
+			p.stepOut()
+	# print(res)
 	print(m.hint)
 	print(m.warn)
 	print(m.left)
+	# print(m.nebr)
+	# print(m.done)
 	print(m.blockCount)
 	print(m.flagCount)
 	m.visualize()
